dde949ef29246c5191288c6795a8e0892e39d0a5
IbraDje <39132283+IbraDje@users.noreply.github.com>
2018-06-12 19:09:59 +0100

    Update testVIBGYOR.py

--- testVIBGYOR.py
+++ testVIBGYOR.py
@@ -3,6 +3,8 @@ import matplotlib.image as mpimg
 import time
 
 name = 'NetHues.jpg'
+# name = 'colorcube-1_66.jpg'
+# name = 'rainbow-page2.jpg'
 img = mpimg.imread(name)
 start_time = time.clock()
 for color in ('v', 'i', 'b', 'g', 'y', 'o', 'r'):
